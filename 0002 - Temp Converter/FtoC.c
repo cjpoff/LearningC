ac40bc6ee3976f4af3dbe8195bbeca08614cbe1e
CJ Poffenberger <{ID}+{username}@users.noreply.github.com>
2025-06-18 19:49:57 -0500

    Removed comments from FtoC.c

--- 0002 - Temp Converter/FtoC.c
+++ 0002 - Temp Converter/FtoC.c
@@ -2,14 +2,10 @@
 
 int main(void)
 {
-    /* Prompt the user to enter a temperature value in Fahrenheit */
     printf("Please enter a temperature value in Fahrenheit: ");
     float temperatureF;
-    /* Read user input into the variable temperatureF */
     scanf("%f", &temperatureF);
-    /* temperatureF read from user input */
     float temperatureC = (temperatureF - 32.0) * 5.0 / 9.0;
-    /* Output the converted temperature in Celsius */
     printf("The corredponding temperature in Celsius is %.2f C. \n", temperatureC);
 
     return 0;
